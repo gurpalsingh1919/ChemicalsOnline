0ee780a0cc3434646bdf9786bb5d961deab43e57
thPiyush1313 <piyushthakur@jwhglobal.com>
2025-10-31 17:14:04 +0000

    server changes

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,11 +14,17 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout','register'],
+'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout','register','/user'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:5173','http://10.1.11.176:7011'],
+ 'allowed_origins' => [
+        'http://10.1.11.176:7011',
+        'http://localhost:5173'
+    ],
 'allowed_headers' => ['*'],
 'supports_credentials' => true,
+ 'exposed_headers' => [],
+
+    'max_age' => 0,
 
 
 ];
